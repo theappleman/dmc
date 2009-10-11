fc06eac3efb5a4fbd928ad8d4d83424846483fb3
pancake@localhost.localdomain <none@none>
2009-10-11 20:36:23 +0200

    * Initial design of the ~/.dmc/acc and ~/.dmc/tmp   - Allows to easily configure accounts for pop3 and imap4   - No support for smtp, and imap4 is incomplete atm * Properly parse input from fifo files * Daemon launch is now implemented, a client can now   be implemented by using echo, :> and cat * Fix output of the dmc-pop3.c parsing   - Single line containing command, return value and status string   - After \n there's the body of the command result   - \x00 is found at the end (EOF character)

--- src/pop3.c
+++ src/pop3.c
@@ -11,10 +11,12 @@
 #include <poll.h>
 
 
+static char *cmd = NULL;
 static char word[1024];
 static char *fifo;
 static int ff;
 
+/* XXX : Use of stdin FILE* fails when using fifo files */
 static char *getword() {
 	fscanf(stdin, "%255s", word);
 	if (feof(stdin))
@@ -31,6 +33,7 @@ static int ready() {
 }
 
 static int waitreply(int lock) {
+	char *ch, *str = word;
 	int ret, reply = -1;
 	fflush(stdout);
 	while(lock || ready()) {
@@ -38,22 +41,41 @@ static int waitreply(int lock) {
 		ret = read(ff, word, 512);
 		if (ret<1)
 			break;
+		word[ret] = 0;
 		if (reply==-1) {
-			if (!memcmp(word, "+OK", 3))
-				reply = 1;
-			else
-			if (!memcmp(word, "-ERR", 4))
-				reply = 0;
+			ch = strchr(str, '\r');
+			if (!ch) ch = strchr(str, '\n');
+			if (ch) {
+				if (!memcmp(word, "+OK", 3))
+					reply = 1;
+				else
+				if (!memcmp(word, "-ERR", 4))
+					reply = 0;
+				*ch = 0;
+				fprintf(stderr, "%s %d \"%s\"\n", cmd, ret, str);
+				str = ch+((ch[1]=='\n')?2:1);
+			}
 		}
-		fprintf(stderr, "\n\n-->(%d)(%s)<--\n\n", ret, word);
+		// TODO: \r \n issues
+		ch = strstr(str, "\r\n.");
+		if (ch)
+			*ch = '\0';
+		fprintf(stderr, "%s\n", str);
 	}
-	fprintf(stderr, "REPLY BOOL IS %d\n", reply);
+	fflush(stderr);
+	write(2, "\x00", 1); // end of output
 	return reply;
 }
 
 static int doword(char *word) {
 	int ret = 1;
-	if (*word == '\0' || !strcmp(word, "exit")) {
+	free (cmd);
+	cmd = strdup(word);
+	//if (*word == '\0' || !strcmp(word, "exit")) {
+	if (*word == '\0') {
+		/* Do nothing */
+	} else
+	if (!strcmp(word, "exit")) {
 		printf("QUIT\n");
 		waitreply(1);
 		ret = 0;
@@ -99,12 +121,27 @@ static void cleanup(int foo) {
 	exit(0);
 }
 
+/* XXX: do not use system here */
+static int storepid(const char *fifo) {
+	int ret;
+	char *cmd = malloc(strlen(fifo)+32);
+	sprintf(cmd, "echo %d > %s.pid", getpid(), fifo);
+	ret = system(cmd);
+	free(cmd);
+	return ret;
+}
+
 int main(int argc, char **argv) {
 	if (argc>1) {
 		signal(SIGINT, cleanup);
 		fifo = argv[1];
 		mkfifo(fifo, 0600);
-		ff = open(fifo, O_RDONLY); //O_NONBLOCK|O_RDWR);
+		ff = open(fifo, O_RDONLY);
+		if (ff == -1) {
+			fprintf(stderr, "Cannot open fifo file.\n");
+			return 1;
+		}
+		storepid(fifo);
 		waitreply(1);
 		while(doword(getword()));
 		cleanup(0);
