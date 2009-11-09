5f1f95053aab71431cadaa0238a43688f9d0de6e
nibble <none@none>
2009-11-09 18:35:20 +0100

    * Added 'dmc-filter -v' to list the value of given headers * Removed dmc -H (replaced by 'dmc-filter -v') * Update Manpage

--- filter.c
+++ filter.c
@@ -6,14 +6,20 @@
 
 int main(int argc, char **argv) {
 	char b[1024], argv2[1024][1024], *ptr;
-	int edit = argc, print = 0, i, j;
+	int edit = argc, filter = 1, value = 0, print = 0, i, j;
 
-	for (i = 0; i < argc; i++)
-		if (!strcmp (argv[i], "-h")) {
-			printf ("Usage: %s [-h] [headers | :] [-e] [new headers] < mail\n", argv[0]);
+	if (argc > 1) {
+		if (!strcmp (argv[1], "-h")) {
+			printf ("Usage: %s [-hv] [headers | :] [-e] [new headers] < mail\n", argv[0]);
 			return 1;
-		} else if (!strcmp (argv[i], "-e"))
-			edit = i;
+		} else if (!strcmp (argv[1], "-v")) {
+			value = 1;
+			filter++;
+		}
+		for (i = filter; i < argc; i++)
+			if (!strcmp (argv[i], "-e"))
+				edit = i;
+	}
 	for (i = 0; i < argc; i++) {
 		strncpy (argv2[i], argv[i], 1023);
 		argv2[i][1023] = '\0';
@@ -23,7 +29,7 @@ int main(int argc, char **argv) {
 	while (fgets (b, 1023, stdin) && b[0] != '\n')
 		if ((b[0] == ' ' || b[0] == '\t')) {
 			if (print) fputs (b, stdout);
-		} else for (i = 1; i < edit && argv[i]; i++)
+		} else for (i = filter; i < edit && argv[i]; i++)
 			if (!strncmp (b, argv[i], strlen(argv[i])) || argv[i][0] == ':') {
 				/* Edit/Remove Headers */
 				print = 1;
@@ -36,15 +42,22 @@ int main(int argc, char **argv) {
 						print = 0;
 						break;
 					}
-				if (print) fputs (b, stdout);
+				if (print) {
+					if (value && (ptr = strchr (b, ':')))
+						ptr += 2;
+					else ptr = b;
+					fputs (ptr, stdout);
+				}
 				break;
 			} else print = 0;
-	/* New Headers */
-	for (i = edit + 1; i < argc; i++)
-		if (argv2[i][0]) puts (argv2[i]);
-	if (edit < argc) puts ("");
-	/* Body */
-	while ((argc < 2 || edit < argc) && fgets (b, 1023, stdin))
-		fputs (b, stdout);
+	if (!value) {
+		/* New Headers */
+		for (i = edit + 1; i < argc; i++)
+			if (argv2[i][0]) puts (argv2[i]);
+		if (edit < argc) puts ("");
+		/* Body */
+		while ((argc < 2 || edit < argc) && fgets (b, 1023, stdin))
+			fputs (b, stdout);
+	}
 	return 0;
 }
